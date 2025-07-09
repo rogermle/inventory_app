25a6e6fd1bc4659a12ad1651eea521a9bf1701e5
Roger Le <rogermle@gmail.com>
2025-07-08 22:10:38 -0500

    Adding postgres db URI to app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from models import db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
 
 app = Flask(__name__)
+
+# Configure PostgreSQL database (default to SQLite if not set)
+POSTGRES_URI = os.environ.get('DATABASE_URL')
+if POSTGRES_URI:
+    app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_URI
+else:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Note: Requires psycopg2 for PostgreSQL
+
+db.init_app(app)
 
 @app.route('/')
 def hello():
@@ -19,4 +37,6 @@ def reports():
     return render_template('reports.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
